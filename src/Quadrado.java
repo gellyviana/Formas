ad7a3d05e46157db7f8d8b8acf8e17a1d4390c58
gellyviana <gellyviana@outlook.com>
2017-04-13 23:21:53 -0300

    Quadrado herda de Retangulo

--- src/Quadrado.java
+++ src/Quadrado.java
@@ -4,5 +4,9 @@ public class Quadrado extends Retangulo implements AreaCalculavel {
 	public Quadrado(double base){
 		super(base, base);
 	}
-
+	public void calcularArea(){
+		
+		double resultado = this.getBase() * this.getBase();
+		System.out.printf("Area do Quadrado: %.2f\n", resultado);
+	}
 }
